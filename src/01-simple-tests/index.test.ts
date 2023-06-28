e4f7e1c8e32e92c727ced8ead3209f9eccaa3b3a
Dzmitry Viartseika <verteyko1990@gmail.com>
2023-06-28 21:44:38 +0200

    # task 1 - implement simple unit tests (simple Calculator)

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,68 @@
-// Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    const data = {
+      a: 2,
+      b: 2,
+      action: Action.Add,
+    }
+    const result = simpleCalculator(data);
+    expect(result).toBe(4);
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    const data = {
+      a: 8,
+      b: 4,
+      action: Action.Subtract,
+    }
+    const result = simpleCalculator(data);
+    expect(result).toBe(4);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    const data = {
+      a: 8,
+      b: 4,
+      action: Action.Multiply,
+    }
+    const result = simpleCalculator(data);
+    expect(result).toBe(32);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    const data = {
+      a: 8,
+      b: 4,
+      action: Action.Divide,
+    }
+    const result = simpleCalculator(data);
+    expect(result).toBe(2);
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    const data = {
+      a: 3,
+      b: 3,
+      action: Action.Exponentiate,
+    }
+    const result = simpleCalculator(data);
+    expect(result).toBe(27);
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    const data = {
+      a: 3,
+      b: 3,
+      action: '$',
+    }
+    const result = simpleCalculator(data);
+    expect(result).toBeNull();
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    const result = simpleCalculator({ a: '2', b: 3, action: Action.Add });
+    expect(result).toBeNull();
   });
 });
